1bf06468f314f7eb4229f97a3fbc540d6a76e8c2
MohammedMawi <90345617+MohammedMawi@users.noreply.github.com>
2024-04-07 18:26:42 -0400

    added projects scroll

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-function copyToClipboard() {
-    var emailText = "mohammedmawi@gmail.com";
-    var tempInput = document.createElement("input");
-    tempInput.value = emailText;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
+document.addEventListener('DOMContentLoaded', function() {
+    function copyToClipboard() {
+        var emailText = "mohammedmawi@gmail.com";
+        var tempInput = document.createElement("input");
+        tempInput.value = emailText;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+    }
 
     document.querySelector('.email').addEventListener('click', function() {
+        copyToClipboard(); // Call the copyToClipboard function when .email is clicked
+
         var tooltip = document.querySelector('.tooltip');
         tooltip.classList.add('show'); // Here we add the 'show' class
     
         setTimeout(function() {
             tooltip.classList.remove('show'); // And here we remove it after 1.5 seconds
-        }, 700);
+        }, 1500);
     });
-}
+
+    document.querySelector('.proj span').addEventListener('click', function() {
+        var myProjectsTitle = document.querySelector('.title');
+        myProjectsTitle.scrollIntoView({ behavior: 'smooth' });
+    });
+});
